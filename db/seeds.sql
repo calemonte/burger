eca75e5fab74f09e5e10d1bfb51d9f948e113a78
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-02-20 09:53:58 -0500

    Add initial files and directories

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (burgerName)
+VALUES 
+    ("Black Bean Veggie Burger"),
+    ("Quarter-pounder with Cheese"),
+    ("Mega Burger with Special Sauce"),
+    ("Quinoa Crunch Veggie Burger"),
+    ("Southwest Burger");
