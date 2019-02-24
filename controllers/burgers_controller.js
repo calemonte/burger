c7d317d8b8e8f8f6dae40f8efdd75ea2eea8b6ae
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-02-24 09:49:23 -0500

    Add basic but functional routing and handlebars main view (partial has been added but not yet functional)

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require("express");
+const burger = require("../models/burger.js");
+
+const router = express.Router();
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var handlebarsObj = {
+      burgers: data
+    };
+    console.log(handlebarsObj);
+    res.render("index", handlebarsObj);
+  });
+});
+
+module.exports = router;
