e287de6d8ca894187bb78c0a4a1e5c837adfd6f0
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-02-24 10:47:36 -0500

    Add put and post routes for controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,4 +15,32 @@ router.get("/", function(req, res) {
   });
 });
 
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burgerName"], [req.body.burgerName], function(result) {
+    // Send back the ID of the newly added burger.
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404.
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
+});
+
 module.exports = router;
