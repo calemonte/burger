6b0f1a84e9f4b27ba55db62c8993e25693ee16de
kwhitebloom <29611438+calemonte@users.noreply.github.com>
2019-02-24 09:04:39 -0500

    Add initial skeleton of burger model

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const orm = require("../config/orm.js");
+
+const burger = {
+  all: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+
+  create: function(cols, val, cb) {
+    orm.insertOne("burgers", cols, val, function(res) {
+      cb(res);
+    });
+  },
+
+  update: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burger;
